d638a33cc2622fbfc708b99ad77c0dcf058a79c2
pdhoward <patrick.howard@hotmail.com>
2021-12-03 13:52:48 -0600

    refactor

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,16 +1,15 @@
 import { BehaviorSubject } from 'rxjs';
 
-export default class UserService {
-  constructor(backend) {
-    this.backend = backend;
-    this.currentUserSubject = new BehaviorSubject(
-      JSON.parse(localStorage.getItem('currentUser'))
-    );
-    this.currentUser = this.currentUserSubject.asObservable();
-    window.addEventListener('storage', this._listenForStorageChanges);
+const UserService = (backend) => {
+  const backend = backend;
+  this.currentUserSubject = new BehaviorSubject(
+    JSON.parse(localStorage.getItem('currentUser'))
+  );
+  const currentUser = currentUserSubject.asObservable();
+    window.addEventListener('storage', _listenForStorageChanges);
   }
 
-  _listenForStorageChanges = (event) => {
+ const  _listenForStorageChanges = (event) => {
     const nextUser = JSON.parse(localStorage.getItem('currentUser'));
     console.log(`In listenForStorageChanges. nextUser = ${nextUser}`);
     if (nextUser !== this.currentUserSubject.value) {
@@ -18,40 +17,40 @@ export default class UserService {
     }
   }
 
-  get currentUserValue() {
+  const currentUserValue = () => {
     return this.currentUserSubject.value;
   }
 
-  login = (email, password) => {
+  const login = (email, password) => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
     };
 
-    return this.backend('/login', requestOptions)
+    return backend('/login', requestOptions)
       .then(this._handleResponse)
       .then(user => {
         // store user details and jwt token in local storage to keep user logged in between page refreshes
         localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
+        currentUserSubject.next(user);
 
         return user;
       });
   };
 
-  logout = () => {
+  const logout = () => {
     localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
+    currentUserSubject.next(null);
   };
 
-  _handleResponse = response => {
+  const _handleResponse = response => {
     return response.text().then(text => {
       const data = text && JSON.parse(text);
       if (!response.ok) {
         if ([401, 403].indexOf(response.status) !== -1) {
           // auto logout if 401 Unauthorized or 403 Forbidden response returned from API
-          this.logout();
+          logout();
           window.location.reload(true);
         }
 
@@ -62,4 +61,6 @@ export default class UserService {
       return data;
     });
   };
-}
+
+
+export default UserService
